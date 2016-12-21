d039dc06492ff192078aaf358ad75cd6db7f20e4
rubyonrailscourse <k.burnard@hotmail.com>
2016-12-21 20:24:35 +0000

    Add edit, update, show and index listing with links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,40 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
    def new
        @article = Article.new
+   end
+   
+   def edit
+       @article = Article.find(params[:id])
    end
    
    def create
       @article = Article.new(article_params)
       
-      if @article.save
-          flash[:notice] = "Article was successfully created!"
-         redirect_to article_path(@article) #redirects the newly created article to the show action
-     else
-         render :new
-    end
+        if @article.save
+            flash[:notice] = "Article was successfully created!"
+            redirect_to article_path(@article) #redirects the newly created article to the show action
+        else
+            render :new
+        end
+   end
+   
+   def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+          flash[:notice] = "Article was successfully updated!"
+          redirect_to article_path(@article)
+      else
+          render :edit
+      end
    end
    
    def show
       @article = Article.find(params[:id])
    end
-   
    
    private
    def article_params
